b973e791483310691d7a46673726f118fd695ff5
Kleponaeru <kelvinlie08@gmail.com>
2026-09-15 17:05:02 +0700

    fix build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,11 @@ rootProject.name = "CloudstreamPlugins"
 // This file sets what projects are included.
 // All new projects should get automatically included unless specified in the "disabled" variable.
 
-val disabled = listOf<String>()
+val disabled = listOf(
+    "Dutamovie",
+    "LayarKaca",
+    "Ngefilm"
+)
 
 File(rootDir, ".").eachDir { dir ->
     if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {
